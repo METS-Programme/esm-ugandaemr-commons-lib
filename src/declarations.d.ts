64103628627c37a405592abba160abdc8b4c6561
Daphne210 <dapnephinerry2@gmail.com>
2023-10-02 12:44:43 +0300

    Fixing errors for lint

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -4,3 +4,16 @@ declare module "*.scss";
 declare module "*.png";
 
 declare type SideNavProps = object;
+
+declare type Indicator = {
+  id: string;
+  label: string;
+  type?: string;
+  attributes?: Array<IndicatorItem>;
+};
+
+declare type IndicatorItem = {
+  id: string;
+  label: string;
+  type?: string;
+};
