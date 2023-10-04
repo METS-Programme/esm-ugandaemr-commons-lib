51e078a8054dc3460924912217b2bbf040f25dd1
Daphne210 <dapnephinerry2@gmail.com>
2023-10-04 15:12:47 +0300

    Adding Temporary fix to resolve webpack issues with imports from the commons library

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,19 @@
-module.exports = require("openmrs/default-webpack-config");
+const path = require("path");
+const config = (module.exports = require("openmrs/default-webpack-config"));
+config.scriptRuleConfig.exclude =
+    path.sep == "/" ?
+    /(node_modules[^\/@openmrs\/esm\-patient\-common\-lib])/ :
+    /(node_modules[^\\@openmrs\/esm\-patient\-common\-lib])/;
+// Temporary fix to resolve webpack issues with imports from the commons library
+config.overrides.resolve = {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".scss"],
+    alias: {
+        "@openmrs/esm-framework": "@openmrs/esm-framework/src/internal",
+        "@ugandaemr/esm-ugandaemr-commons-lib": path.resolve(
+            __dirname,
+            "./src/index"
+        ),
+        "@openmrs/openmrs-form-engine-lib": "@openmrs/openmrs-form-engine-lib/src/index",
+    },
+};
+module.exports = config;
