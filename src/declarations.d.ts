9c8b530b931d56c0d6b47111d70dbb21a38d91c7
Daphne <89844768+Daphne210@users.noreply.github.com>
2023-10-16 11:29:32 +0300

    Moving Indicator from declarations to the component (#14)
    * Making the icon on the header configurable
    
    * Moving Indicator from declarations to the component

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -2,18 +2,3 @@ declare module "@carbon/react";
 declare module "*.css";
 declare module "*.scss";
 declare module "*.png";
-
-declare type SideNavProps = object;
-
-declare type Indicator = {
-  id: string;
-  label: string;
-  type?: string;
-  attributes?: Array<IndicatorItem>;
-};
-
-declare type IndicatorItem = {
-  id: string;
-  label: string;
-  type?: string;
-};
